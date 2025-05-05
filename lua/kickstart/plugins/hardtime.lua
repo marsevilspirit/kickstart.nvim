f9b33b14b9b9e9dae1a48ab6f2a3c2cff28fefe3
marsevilspirit <marsevilspirit@gmail.com>
2025-05-05 22:27:58 +0800

    update

--- lua/kickstart/plugins/hardtime.lua
+++ lua/kickstart/plugins/hardtime.lua
@@ -1,5 +1,10 @@
 return {
   'm4xshen/hardtime.nvim',
   dependencies = { 'MunifTanjim/nui.nvim' },
-  opts = {},
+  config = function()
+    require('hardtime').setup {
+      max_time = 10000,
+      max_count = 50,
+    }
+  end,
 }
