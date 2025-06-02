cd205120daef4f6940b37e941adcb2a593f9d740
marsevilspirit <marsevilspirit@gmail.com>
2025-06-02 13:49:54 +0800

    update nvim

--- init.lua
+++ init.lua
@@ -1050,6 +1050,7 @@ require('lazy').setup({
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.snacks',
   require 'kickstart.plugins.music',
+  require 'kickstart.plugins.zen',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
